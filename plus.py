31c124ea40320c75fb013781621692229d876a7b
Divay <divay7@gmail.com>
2022-09-05 22:36:43 -0400

    Fixed skeletal code

--- plus.py
+++ plus.py
@@ -7,9 +7,9 @@ num_merge = int(input("Please enter the number of PPTs you want to merge: "))
 fpath_1 = input("Please enter the path to the first file: ")
 
 # Open file for reading
-file_main = open(file_path)
-prs_main = Presentation(open_file)
+file_main = open(fpath_1)
+prs_main = Presentation(file_main)
 
-for _ in range(1, num_merge-1):
-	fpath_2 = input("Please enter the path to the next file: ")
-	print(fpath2)
+# for _ in range(1, num_merge-1):
+# 	fpath_2 = input("Please enter the path to the next file: ")
+# 	print(fpath2)
